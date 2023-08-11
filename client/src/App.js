ff4a72d2d98417b635b543a634bbe025eee98995
jbtokam <85589835+jeanbTokam@users.noreply.github.com>
2023-08-10 22:20:06 -0400

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 function App() {
 
-  const [input, setInput]= useState("")
-  const[todos, setTodos] = useState([])
+  const [input, setInput] = useState("")
+  const [todos, setTodos] = useState([])
 
-const addTodo = (e) =>{
-  if(input.trim() === '') return
-  else{
-  e.preventDefault()
-  setTodos([...todos, {id: uuidv4(), title: input}])
-  setInput("")
+  const addTodo = (e) => {
+    if (input.trim() === '') return
+    else {
+      e.preventDefault()
+      setTodos([...todos, { id: uuidv4(), title: input }])
+      setInput("")
+    }
   }
-}
-const removeTodo =(id)=>{
-  setTodos(todos.filter((todo)=> todo.id !== id))
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
 
-}
+  }
 
 
   return (
     <div className="App">
       <h1>Todo App</h1>
-     <div className="container">
+      <div className="container">
         <form onSubmit={addTodo} >
 
-          <input type="text" 
-          name="input" 
-          value={input}
-          onChange={(e)=> setInput(e.target.value)}
+          <input type="text"
+            name="input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
-           <button type="submit">add Todo</button>
+          <button type="submit">add Todo</button>
         </form>
         <div>
-          {todos.map((todo)=>(
-            <>
-            <p key={todo.id} className='todo'>{todo.title}
-            
-            <i 
-            className="fa-solid fa-trash"
-            onClick={()=>removeTodo(todo.id)}>
-              </i>
-            </p>
-          
-              
-              </>
+          {todos.map((todo) => (
+            <div key={todo.id} className='todo'>
+              <p key={todo.id} >{todo.title}
+
+                <i
+                  className="fa-solid fa-trash"
+                  onClick={() => removeTodo(todo.id)}>
+                </i>
+              </p>
+              </div>
           ))}
         </div>
-           <button type=""></button>
-        
-        </div>
+        <button type=""></button>
+
+      </div>
     </div>
   );
 }
